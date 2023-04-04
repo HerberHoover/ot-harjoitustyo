8a200b1f5ae01ee5ffb4cc874a0ead1363b44cdc
Herber Hoover <elias.kousa@outlook.com>
2023-04-04 09:58:33 +0300

    __init__.py

---
+++ src/app/user_interface/__init__.py
@@ -0,0 +1,4 @@
+
+from .login_view import LoginView
+from .register_view import RegisterView
+from .home_view import HomeView
---
+++ src/database/__init__.py
@@ -0,0 +1,3 @@
+from .create_database import create_tables  # Updated import statement
+
+
