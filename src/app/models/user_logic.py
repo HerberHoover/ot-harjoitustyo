5ea71bd9f32389ad5b9ba6a63daf1e5edbd1baf6
Herber Hoover <elias.kousa@outlook.com>
2023-04-04 09:57:27 +0300

    user logic added

---
+++ src/app/models/user_logic.py
@@ -0,0 +1,27 @@
+from tkinter import messagebox
+from database.user import verify_user, create_user
+
+def login(username, password):
+    if not username or not password:
+        messagebox.showerror("Error", "Please enter both username and password.")
+        return False
+
+    if verify_user(username, password):
+        messagebox.showinfo("Success", "You have successfully logged in.")
+        return True
+    else:
+        messagebox.showerror("Error", "Invalid username or password.")
+        return False
+
+def register(username, password, confirm_password):
+    if not username or not password or not confirm_password:
+        messagebox.showerror("Error", "Please enter all fields.")
+        return False
+
+    if password != confirm_password:
+        messagebox.showerror("Error", "Passwords do not match.")
+        return False
+
+    create_user(username, password)
+    messagebox.showinfo("Success", "Registration successful.")
+    return True
