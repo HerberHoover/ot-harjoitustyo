9b1db3f80dd6f1e29dd9c726143b43b85dff327f
Herber Hoover <elias.kousa@outlook.com>
2023-05-02 15:53:44 +0300

    get category added

--- src/app/models/category_controller.py
+++ src/app/models/category_controller.py
@@ -1,8 +1,11 @@
-from database.categories import add_category as db_add_category
+from database.categories import add_category, get_categories
 
 class CategoryController:
     def __init__(self, user_id):
         self.user_id = user_id
 
     def add_category(self, category_name):
-        db_add_category(self.user_id, category_name)
+        add_category(self.user_id, category_name)
+
+    def get_categories(self):
+        return get_categories(self.user_id)
